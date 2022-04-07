0dbf98a429520cc1f9e7781b721f79d4b74888fc
ARPIT KUMAR TAPASWI <arpit.tapaswi@live.com>
2022-04-07 10:10:48 +0530

    Create post logic implemented in backend.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,23 @@
-export const getPosts = (req, res) => {
-  res.send("Califer you are amazing backend developer");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find();
+
+    console.log(postMessage);
+
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPosts = async (req, res) => {
+  const post = new PostMessage(req.body);
+  try {
+    await post.save();
+    res.status(201).json(post);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { getPosts } from "../controllers/posts.js";
+import { getPosts , createPosts } from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", createPosts);
+
 
 export default router;

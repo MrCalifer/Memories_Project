9a64a2b98a29658aa45d3177ea83bb7ce34868e3
ARPIT KUMAR TAPASWI <arpit.tapaswi@live.com>
2022-04-09 11:22:13 +0530

    React-Redux setup completed.

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

f7b87c791e79c2c611d27f5d557844d659b91bb5
ARPIT KUMAR TAPASWI <arpit.tapaswi@live.com>
2022-06-08 02:36:42 +0530

    OAuth Bug Fixing

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -12,7 +12,8 @@ export const AUTH = 'AUTH';
 export const LOGOUT = 'LOGOUT'
 export const SIGNIN = 'SIGN-IN'
 export const SIGNUP = 'SIGN-UP'
-export const GOOGLE_CLIENT_ID = '590422828231-8maeee4ifm33bselk7msv5d7ngudqjvt.apps.googleusercontent.com'
+// export const GOOGLE_CLIENT_ID = '590422828231-8maeee4ifm33bselk7msv5d7ngudqjvt.apps.googleusercontent.com'
+export const GOOGLE_CLIENT_ID = '139977198196-oiq05dhb3m69qihm59qlfhevnv6oec21.apps.googleusercontent.com'
 
 //Extra
 export const START_LOADING = "START_LOADING";
